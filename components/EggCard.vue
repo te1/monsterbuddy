0a8a7b31af7dc5d040a895522c0f32db03a824cf
te1 <te1@users.noreply.github.com>
2021-07-17 02:05:56 +0200

    eggs

--- components/EggCard.vue
+++ components/EggCard.vue
@@ -5,29 +5,58 @@
     <img
       :src="require(`~/assets/eggs/${monster.name}.svg`)"
       :alt="monster.name"
-      class="h-[110px] my-2"
+      class="h-[120px] my-2"
     >
 
-    <div class="flex-1 self-start1 mx-4 my-2 flex flex-col text-gray-600 font-semibold tracking-wide">
-      <div class="text-gray-400 leading-none">
+    <div class="h-full flex-1 mx-4 py-3 flex flex-col text-gray-600 font-semibold tracking-wide">
+      <div class="text-gray-400 leading-tight">
         {{ formattedNo }}
       </div>
 
-      <div class="text-black text-xl">
+      <div class="flex-1 text-black text-xl">
         {{ monster.name }}
       </div>
 
-      <div>
-        {{ monster.genus }}
+      <div class="flex items-center mb-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-gray-400"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z"
+            clip-rule="evenodd"
+          />
+        </svg>
+
+        <div class="ml-1">
+          {{ monster.genus }}
+        </div>
       </div>
 
-      <div>
-        {{ monster.habitat }}
+      <div class="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-gray-400"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <div class="ml-1">
+          {{ monster.habitat }}
+        </div>
       </div>
     </div>
 
     <MonsterImage
-      class="h-[110px] m-2"
+      class="h-[120px] m-2"
       :monster="monster"
     />
   </div>
