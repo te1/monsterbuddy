7568c79ab2aaf2770ccd8fd1915b6197f1aa49df
te1 <te1@users.noreply.github.com>
2021-07-24 12:56:14 +0200

    add more data, key data for faster access, add related monsters, freeze data for performance

--- services/data.js
+++ services/data.js
@@ -1,32 +1,47 @@
 import _ from 'lodash';
+import { deepFreeze } from './utils';
 import monsters from '~/assets/data/monsters';
 
+_.forEach(monsters, (monster) => {
+  monster.slug = monster.name
+    .replace(/ /g, '-')
+    .replace(/\(|\)/g, '')
+    .toLowerCase();
+});
+
+deepFreeze(monsters);
+
 export { monsters };
+export const monstersByNo = Object.freeze(_.keyBy(monsters, 'no'));
+export const monstersByName = Object.freeze(_.keyBy(monsters, 'name'));
+export const monstersBySlug = Object.freeze(_.keyBy(monsters, 'slug'));
 export const monsties = getMonstersByHatchable(true);
 export const genera = getGenera();
 export const habitats = getHabitats();
 
 export function getGenera(monsterList = monsters) {
-  return _.sortBy(_.uniq(_.map(monsterList, 'genus')));
+  return deepFreeze(_.sortBy(_.uniq(_.map(monsterList, 'genus'))));
 }
 
 export function getHabitats(monsterList = monsters) {
-  return _.sortBy(_.uniq(_.map(monsterList, 'habitat')));
+  return deepFreeze(_.sortBy(_.uniq(_.map(monsterList, 'habitat'))));
 }
 
 export function getMonstersByGenus(genus, monsterList = monsters) {
-  return _.filter(monsterList, { genus });
+  return deepFreeze(_.filter(monsterList, { genus }));
 }
 
 export function getMonstersByHabitat(habitat, monsterList = monsters) {
-  return _.filter(monsterList, { habitat });
+  return deepFreeze(_.filter(monsterList, { habitat }));
 }
 
 export function getMonstersByHatchable(hatchable, monsterList = monsters) {
-  return _.filter(monsters, (monster) => {
-    if (hatchable) {
-      return monster.hatchable === hatchable;
-    }
-    return !monster.hatchable;
-  });
+  return deepFreeze(
+    _.filter(monsterList, (monster) => {
+      if (hatchable) {
+        return monster.hatchable === hatchable;
+      }
+      return !monster.hatchable;
+    })
+  );
 }
--- services/utils.js
+++ services/utils.js
@@ -1,5 +1,21 @@
 import _ from 'lodash';
 
+export function deepFreeze(object) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+
+  const propNames = Object.getOwnPropertyNames(object);
+
+  for (const name of propNames) {
+    const value = object[name];
+
+    if (value && typeof value === 'object') {
+      deepFreeze(value);
+    }
+  }
+
+  return Object.freeze(object);
+}
+
 export function formatNo(no, withHash = true) {
   let result = _.padStart(no, 3, '0');
 
