949c8313428b1bc15ce3ef77963ab9c9f440ba0c
te1 <te1@users.noreply.github.com>
2021-08-12 01:31:23 +0200

    blur fab after tap

--- components/app/AppFloatingButton.vue
+++ components/app/AppFloatingButton.vue
@@ -4,7 +4,7 @@
       type="button"
       class="fixed bottom-16 right-4 z-10 flex items-center justify-center w-12 h-12 rounded-full drop-shadow-md text-2xl cursor-pointer transition-colors bg-brand-500 text-white dark:text-cool-200 hover:bg-brand-600 active:bg-gray-500 dark:active:bg-cool-500"
       :title="title"
-      @click="$emit('click')"
+      @click="onClick"
     >
       <slot />
     </button>
@@ -20,6 +20,17 @@
         type: String,
         required: false,
         default: null,
+      },
+    },
+
+    methods: {
+      onClick() {
+        this.$emit('click');
+
+        if (document.activeElement) {
+          // blur to disable active/focus styles
+          document.activeElement.blur();
+        }
       },
     },
   };
