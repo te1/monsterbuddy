9e0edc9f2364b0c2a9faed5d6ea7b9e3ec2f0853
te1 <te1@users.noreply.github.com>
2021-08-18 21:43:29 +0200

    make the filter button easier to hit

--- components/app/AppFloatingButton.vue
+++ components/app/AppFloatingButton.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="relative">
+    <div class="fixed bottom-16 right-4 z-10 w-12 h-12 scale-125" />
+
     <button
       type="button"
       class="fixed bottom-16 right-4 z-10 flex items-center justify-center w-12 h-12 rounded-full drop-shadow-md text-2xl cursor-pointer transition-colors bg-brand-500 text-white dark:text-cool-200 hover:bg-brand-600 active:bg-gray-500 dark:active:bg-cool-500"
