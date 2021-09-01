a07080a875d029095ee52a514c229567ad16882c
te1 <te1@users.noreply.github.com>
2021-09-02 00:09:20 +0200

    add borders to nav links

--- components/app/AppNav.vue
+++ components/app/AppNav.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="fixed bottom-0 w-full h-12 z-20 shadow-top border-t border-gray-300 bg-white dark:border-cool-600 dark:bg-cool-800">
-    <nav class="container mx-auto px-4 h-full grid grid-flow-col auto-cols-fr gap-4 select-none">
+    <nav class="container mx-auto px-4 h-full grid grid-flow-col auto-cols-fr divide-x divide-gray-300 dark:divide-cool-600 select-none">
       <AppNavLink
         to="/monsters/"
         text="Monsters"
