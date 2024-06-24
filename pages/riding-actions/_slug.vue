8f4d6dfcf7d63f74bb1808cd550c5f90bb0c8fd7
te <te1@users.noreply.github.com>
2024-06-24 23:21:13 +0200

    fix fallback URL, formatting

--- pages/riding-actions/_slug.vue
+++ pages/riding-actions/_slug.vue
@@ -3,7 +3,7 @@
     <AppTopBar
       :heading="ridingAction.name"
       showBack
-      backFallback="/catavan-stands/"
+      backFallback="/riding-actions/"
     />
 
     <AppFloatingButton
@@ -28,9 +28,7 @@
           :icon="['fas', 'dna']"
         />
 
-        <div class="font-semibold mb-1">
-          Learnt By
-        </div>
+        <div class="font-semibold mb-1">Learnt By</div>
       </div>
 
       <div class="grid gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -46,119 +44,119 @@
 </template>
 
 <script>
-  import _ from 'lodash';
-  import {
-    ridingActionsBySlug,
-    getMonstiesByRidingAction,
-  } from '~/services/data';
-  import { makeHead } from '~/services/utils';
+import _ from 'lodash';
+import {
+  ridingActionsBySlug,
+  getMonstiesByRidingAction,
+} from '~/services/data';
+import { makeHead } from '~/services/utils';
 
-  export default {
-    name: 'PageRidingActionDetails',
+export default {
+  name: 'PageRidingActionDetails',
 
-    data() {
-      return {
-        ridingAction: ridingActionsBySlug[this.$route.params.slug],
-        display: null,
-      };
+  data() {
+    return {
+      ridingAction: ridingActionsBySlug[this.$route.params.slug],
+      display: null,
+    };
+  },
+
+  head() {
+    if (this.ridingAction == null) {
+      return;
+    }
+
+    let description = `${this.ridingAction.name} is a riding action that `;
+
+    if (this.monsters.length) {
+      description += `${this.monsters.length} `;
+    }
+
+    description += 'monsties can learn.';
+
+    switch (this.ridingAction.type) {
+      case 'utility':
+        description += ' It provides general utility.';
+        break;
+
+      case 'explore':
+        description +=
+          ' It helps with exploration and accessing treasure chests in dens.';
+        break;
+
+      case 'search':
+        description += ' It helps finding monsters, dens or materials.';
+    }
+
+    return makeHead({
+      title: `Monster Buddy - ${this.ridingAction.name} In Monster Hunter Stories 2`,
+      description,
+      canonical: `https://monsterbuddy.app/riding-actions/${this.ridingAction.slug}/`,
+    });
+  },
+
+  computed: {
+    monsters() {
+      return getMonstiesByRidingAction(this.ridingAction.name);
     },
 
-    head() {
-      if (this.ridingAction == null) {
-        return;
+    displays() {
+      return ['monstie', 'egg'];
+    },
+
+    nextDisplay() {
+      let currentIndex = _.indexOf(this.displays, this.display);
+      let nextIndex = (currentIndex + 1) % this.displays.length;
+
+      return this.displays[nextIndex];
+    },
+
+    fabVisible() {
+      return this.displays.length > 1;
+    },
+
+    fabTitle() {
+      switch (this.nextDisplay) {
+        case 'monstie':
+          return 'Show monsties ';
+
+        case 'egg':
+          return 'Show eggs';
+
+        default:
+          return null;
       }
+    },
 
-      let description = `${this.ridingAction.name} is a riding action that `;
+    fabIcon() {
+      switch (this.nextDisplay) {
+        case 'monstie':
+          return ['fas', 'dragon'];
 
-      if (this.monsters.length) {
-        description += `${this.monsters.length} `;
+        case 'egg':
+          return ['fas', 'egg'];
+
+        default:
+          return null;
       }
+    },
+  },
 
-      description += 'monsties can learn.';
-
-      switch (this.ridingAction.type) {
-        case 'utility':
-          description += ' It provides general utility.';
-          break;
-
-        case 'explore':
-          description +=
-            ' It helps with exploration and accessing treasure chests in dens.';
-          break;
-
-        case 'search':
-          description += ' It helps finding monsters, dens or materials.';
-      }
-
-      return makeHead({
-        title: `Monster Buddy - ${this.ridingAction.name} In Monster Hunter Stories 2`,
-        description,
-        canonical: `https://monsterbuddy.app/riding-actions/${this.ridingAction.slug}/`,
+  created() {
+    if (this.ridingAction == null) {
+      this.$nuxt.error({
+        statusCode: 404,
+        message: 'This page could not be found',
       });
+    } else {
+      this.toggleDisplay();
+    }
+  },
+
+  methods: {
+    toggleDisplay() {
+      this.display = this.nextDisplay;
     },
-
-    computed: {
-      monsters() {
-        return getMonstiesByRidingAction(this.ridingAction.name);
-      },
-
-      displays() {
-        return ['monstie', 'egg'];
-      },
-
-      nextDisplay() {
-        let currentIndex = _.indexOf(this.displays, this.display);
-        let nextIndex = (currentIndex + 1) % this.displays.length;
-
-        return this.displays[nextIndex];
-      },
-
-      fabVisible() {
-        return this.displays.length > 1;
-      },
-
-      fabTitle() {
-        switch (this.nextDisplay) {
-          case 'monstie':
-            return 'Show monsties ';
-
-          case 'egg':
-            return 'Show eggs';
-
-          default:
-            return null;
-        }
-      },
-
-      fabIcon() {
-        switch (this.nextDisplay) {
-          case 'monstie':
-            return ['fas', 'dragon'];
-
-          case 'egg':
-            return ['fas', 'egg'];
-
-          default:
-            return null;
-        }
-      },
-    },
-
-    created() {
-      if (this.ridingAction == null) {
-        this.$nuxt.error({
-          statusCode: 404,
-          message: 'This page could not be found',
-        });
-      } else {
-        this.toggleDisplay();
-      }
-    },
-
-    methods: {
-      toggleDisplay() {
-        this.display = this.nextDisplay;
-      },
-    },
-  };
+  },
+};
 </script>
