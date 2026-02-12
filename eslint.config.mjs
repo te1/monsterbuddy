5c8c9577a413ffef390797fbd3cea8b6cad9fe6f
te <te1@users.noreply.github.com>
2026-02-13 00:58:54 +0100

    update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default withNuxt(
+  {
+    'vue/attribute-hyphenation': ['error', 'never'],
+  },
   // Your custom configs here
   eslintConfigPrettier
 );
