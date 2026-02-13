e2c4222902e3ac1de0b777ff9c06e441dba01b06
te <te1@users.noreply.github.com>
2026-02-13 01:17:01 +0100

    remove placeholder content

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -1,7 +1,86 @@
 <script setup lang="ts">
   import type { ContentNavigationItem } from '@nuxt/content';
 
-  const navigation = inject<Ref<ContentNavigationItem[]>>('navigation');
+  const navigation: ContentNavigationItem[] = [
+    {
+      title: 'Getting Started',
+      path: '/getting-started',
+      stem: '1.getting-started/1.index',
+      children: [
+        {
+          title: 'Introduction',
+          path: '/getting-started',
+          stem: '1.getting-started/1.index',
+          icon: 'i-lucide-house',
+        },
+        {
+          title: 'Installation',
+          path: '/getting-started/installation',
+          stem: '1.getting-started/2.installation',
+          icon: 'i-lucide-download',
+        },
+        {
+          title: 'Usage',
+          path: '/getting-started/usage',
+          stem: '1.getting-started/3.usage',
+          icon: 'i-lucide-sliders',
+        },
+      ],
+      icon: false,
+    },
+    {
+      title: 'Essentials',
+      path: '/essentials',
+      stem: '2.essentials',
+      children: [
+        {
+          title: 'Markdown Syntax',
+          path: '/essentials/markdown-syntax',
+          stem: '2.essentials/1.markdown-syntax',
+          icon: 'i-lucide-heading-1',
+        },
+        {
+          title: 'Code Blocks',
+          path: '/essentials/code-blocks',
+          stem: '2.essentials/2.code-blocks',
+          icon: 'i-lucide-code-xml',
+        },
+        {
+          title: 'Prose Components',
+          path: '/essentials/prose-components',
+          stem: '2.essentials/3.prose-components',
+          icon: 'i-lucide-component',
+        },
+        {
+          title: 'Images and Embeds',
+          path: '/essentials/images-embeds',
+          stem: '2.essentials/4.images-embeds',
+          icon: 'i-lucide-image',
+        },
+      ],
+      page: false,
+    },
+    {
+      title: 'AI',
+      path: '/ai',
+      stem: '3.ai',
+      children: [
+        {
+          title: 'MCP Server',
+          path: '/ai/mcp',
+          stem: '3.ai/1.mcp',
+          icon: 'i-lucide-cpu',
+        },
+        {
+          title: 'LLMs Integration',
+          path: '/ai/llms',
+          stem: '3.ai/2.llms',
+          icon: 'i-lucide-message-circle-code',
+        },
+      ],
+      page: false,
+    },
+  ];
 </script>
 
 <template>
---
+++ app/pages/[...slug].vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'docs',
+  });
+
+  const title = 'Introduction';
+  const description = 'Welcome to Nuxt UI documentation template.';
+
+  useSeoMeta({
+    title,
+    ogTitle: title,
+    description,
+    ogDescription: description,
+  });
+
+  const headline = 'Getting Started';
+
+  defineOgImageComponent('Docs', {
+    headline: headline,
+  });
+
+  const links = [
+    {
+      icon: 'i-lucide-external-link',
+      label: 'Edit this page',
+      text: 'Edit this page',
+      to: `link`,
+      target: '_blank',
+      id: 'edit',
+      depth: 1,
+    },
+  ];
+</script>
+
+<template>
+  <UPage>
+    <UPageHeader :title="title" :description="description" :headline="headline">
+      <template #links>
+        <UButton v-for="(link, index) in links" :key="index" v-bind="link" />
+
+        <PageHeaderLinks />
+      </template>
+    </UPageHeader>
+
+    <UPageBody>
+      <div>CONTENT</div>
+    </UPageBody>
+
+    <template v-if="links?.length" #right>
+      <UContentToc :title="title" :links="links">
+        <template #bottom>
+          <div class="hidden space-y-6 lg:block" :class="{ 'mt-6!': links?.length }">
+            <USeparator v-if="links?.length" type="dashed" />
+
+            <UPageLinks :title="title" :links="links" />
+          </div>
+        </template>
+      </UContentToc>
+    </template>
+  </UPage>
+</template>
