f422165fb9b6c49e26097ffcf9c983a188cfc97e
te <te1@users.noreply.github.com>
2026-02-21 23:27:40 +0100

    nav

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,15 +1,9 @@
 <script lang="ts" setup>
   const navItems = computed(() => getNavItems(useRoute().path));
-  const navItemsFlat = computed(() =>
-    navItems.value.flatMap((item) =>
-      item.label === 'More' && item.children ? item.children : [item]
-    )
-  );
 </script>
 
 <template>
-  <UHeader to="/" mode="drawer" toggleSide="left" :ui="{ center: 'flex-1' }">
-    <!-- <AppSearchButton :collapsed="false" class="w-full" /> -->
+  <UHeader to="/" :toggle="false" :ui="{ center: 'flex-1' }">
     <UNavigationMenu :items="navItems" variant="link" contentOrientation="vertical" />
 
     <template #left>
@@ -19,13 +13,9 @@
     </template>
 
     <template #right>
-      <AppSearchButton />
       <UColorModeButton />
-      <GameMenu />
-    </template>
-
-    <template #body>
-      <UNavigationMenu :items="navItemsFlat" orientation="vertical" class="-mx-2.5" />
+      <AppSearchButton />
+      <AppGameSelect />
     </template>
   </UHeader>
 </template>
--- app/components/AppMobileBottomNav.vue
+++ app/components/AppMobileBottomNav.vue
@@ -14,7 +14,12 @@
       :items="navItems"
       variant="link"
       contentOrientation="vertical"
-      :popover="{ content: { side: 'top' } }"
+      :ui="{
+        viewportWrapper:
+          'absolute bottom-full! top-auto! left-0 right-0 flex justify-end',
+        viewport:
+          'left-auto! right-0! w-60 max-w-[calc(100vw-0.5rem)] sm:w-(--reka-navigation-menu-viewport-width)'
+      }"
     />
   </UHeader>
 </template>
