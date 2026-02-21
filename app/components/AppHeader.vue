1abc981b65c8be900d3228582c6f185595710030
te <te1@users.noreply.github.com>
2026-02-21 21:37:34 +0100

    nav

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,16 @@
+<script lang="ts" setup>
+  const navItems = computed(() => getNavItems(useRoute().path));
+  const navItemsFlat = computed(() =>
+    navItems.value.flatMap((item) =>
+      item.label === 'More' && item.children ? item.children : [item]
+    )
+  );
+</script>
+
 <template>
-  <UHeader :ui="{ center: 'flex-1' }" to="/">
+  <UHeader to="/" mode="drawer" toggleSide="left" :ui="{ center: 'flex-1' }">
     <!-- <AppSearchButton :collapsed="false" class="w-full" /> -->
-    <AppNav />
+    <UNavigationMenu :items="navItems" variant="link" contentOrientation="vertical" />
 
     <template #left>
       <NuxtLink to="/">
@@ -13,6 +22,10 @@
       <AppSearchButton />
       <UColorModeButton />
       <GameMenu />
+    </template>
+
+    <template #body>
+      <UNavigationMenu :items="navItemsFlat" orientation="vertical" class="-mx-2.5" />
     </template>
   </UHeader>
 </template>
--- app/components/AppNav.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-  const items = computed(() => getNavItems(useRoute().path));
-</script>
-
-<template>
-  <UNavigationMenu :items="items" variant="link" contentOrientation="vertical" />
-</template>
