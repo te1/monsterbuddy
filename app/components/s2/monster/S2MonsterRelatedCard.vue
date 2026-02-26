93e80e49bea56a793e66dff23c8164b8a7d7f54a
te <te1@users.noreply.github.com>
2026-02-26 21:48:23 +0100

    types

--- app/components/s2/monster/S2MonsterRelatedCard.vue
+++ app/components/s2/monster/S2MonsterRelatedCard.vue
@@ -1,24 +1,33 @@
 <script lang="ts" setup>
-  import type { Monster } from '~/services/2/types';
+  import type { Monster, RelationType } from '~/services/2/types';
   import { monstersByName } from '~/services/2/data';
 
   const props = defineProps<{ monster: Monster }>();
 
-  const hasRelated = computed(() => !!props.monster?.related?.length);
+  const relations = computed(() => {
+    return (props.monster.related ?? [])
+      .map((relation) => {
+        return {
+          type: relation.type,
+          monster: monstersByName.get(relation.monster),
+        };
+      })
+      .filter(
+        (relation): relation is { type: RelationType; monster: Monster } => relation.monster != null
+      );
+  });
 
-  function getMonsterByName(name: string) {
-    return monstersByName.get(name);
-  }
+  const hasRelated = computed(() => relations.value.length > 0);
 </script>
 
 <template>
   <section v-if="hasRelated">
     <h3 class="text-lg font-semibold">Related</h3>
 
-    <div v-for="(relation, index) in monster.related" :key="relation.monster">
+    <div v-for="(relation, index) in relations" :key="relation.monster.name">
       <div v-if="index > 0" class="my-2 border-t border-neutral-300 dark:border-neutral-600" />
 
-      <S2MonsterRelatedItem :monster="getMonsterByName(relation.monster)" />
+      <S2MonsterRelatedItem :monster="relation.monster" />
     </div>
   </section>
 </template>
