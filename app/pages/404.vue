405994ad8eccd9dfd7a37a6b3d3b34baf0dcfecd
te <te1@users.noreply.github.com>
2026-03-01 05:20:12 +0100

    404

---
+++ app/pages/404.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+  const { title } = useAppConfig().seo;
+
+  definePageMeta({
+    layout: false,
+  });
+
+  useSeoMeta({
+    title: `Page Not Found`,
+    description: 'We are sorry but there was an error with this page.',
+    robots: 'noindex, follow',
+  });
+</script>
+
+<template>
+  <UError
+    :error="{
+      statusCode: 404,
+      statusMessage: 'Page Not Found',
+    }"
+    :clear="{
+      label: `Back to ${title}`,
+      size: 'xl',
+      color: 'neutral',
+      variant: 'link',
+      icon: 'i-lucide-arrow-left',
+      class: 'cursor-pointer',
+    }"
+  />
+
+  <!-- TODO add search box -->
+</template>
