fba541bd86a15bdcc2559cd35b72544a33d3a47b
te <te1@users.noreply.github.com>
2026-03-03 02:09:45 +0100

    pages

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,34 @@
 <script lang="ts" setup>
   const { description, title } = useAppConfig().seo;
 
-  useHead({
+  const route = useRoute();
+  const gameType = computed(() => routePathToGameType(route.path));
+  const icon = computed(() => {
+    switch (gameType.value) {
+      case 'mhst1':
+        return '/s1.ico';
+
+      case 'mhst2':
+        return '/s2.ico';
+
+      case 'mhst3':
+        return '/s3.ico';
+
+      default:
+        return '/favicon.ico';
+    }
+  });
+
+  useHead(() => ({
     meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    link: [{ rel: 'icon', href: '/favicon.ico' }],
+    link: [{ key: 'app-favicon', rel: 'icon', href: icon.value }],
     htmlAttrs: {
       lang: 'en',
     },
     bodyAttrs: {
       class: 'bg-elevated',
     },
-  });
+  }));
 
   useSeoMeta({
     titleTemplate: `%s - ${title}`,
