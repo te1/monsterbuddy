e838da0749b4fe01b4fa2bdb834cd8b32ca0de09
te <te1@users.noreply.github.com>
2026-03-03 23:40:29 +0100

    layout

--- app/pages/1/index.vue
+++ app/pages/1/index.vue
@@ -11,15 +11,45 @@
 </script>
 
 <template>
-  <UContainer>
-    <UPage>
-      <UPageHeader
-        :title="title"
-        description="Monster Buddy is a companion app for Monster Hunter Stories 1 that can help you learn or remember information useful during combat and general gameplay. It's not a complete guide, walkthrough or advanced character / gene planner (yet). You can use it for all versions like the Remastered, iOS, Android and original on the Nintendo 3DS."
-        :headline="headline"
-      />
+  <div>
+    <UPageHeader
+      :title="title"
+      description="Monster Buddy is a companion app for Monster Hunter Stories 1 that can help you learn or remember information useful during combat and general gameplay. It's not a complete guide, walkthrough or advanced character / gene planner (yet). You can use it for all versions like the Remastered, iOS, Android and original on the Nintendo 3DS."
+      :headline="headline"
+    />
 
-      <UPageBody>Content</UPageBody>
-    </UPage>
-  </UContainer>
+    <UPageBody>
+      <UPageGrid>
+        <UPageCard to="/1/monsters" title="Monster list" :ui="{ container: 'sm:p-4' }">
+          <template #description>
+            <ProseUl class="my-0">
+              <ProseLi>Attack patterns</ProseLi>
+              <ProseLi>Elemental weaknesses</ProseLi>
+            </ProseUl>
+          </template>
+        </UPageCard>
+
+        <UPageCard to="/1/monsties" title="Monstie list" :ui="{ container: 'sm:p-4' }">
+          <template #description>
+            <ProseUl class="my-0">
+              <ProseLi>Attack tendencies</ProseLi>
+              <ProseLi>Retreat conditions</ProseLi>
+              <ProseLi>Locations</ProseLi>
+              <ProseLi>Stats</ProseLi>
+            </ProseUl>
+          </template>
+        </UPageCard>
+
+        <UPageCard to="/1/eggs" title="Visual egg list" :ui="{ container: 'sm:p-4' }">
+          <template #description>
+            <ProseUl class="my-0">
+              <ProseLi>Search</ProseLi>
+              <ProseLi>Sorting</ProseLi>
+              <ProseLi>Filtering</ProseLi>
+            </ProseUl>
+          </template>
+        </UPageCard>
+      </UPageGrid>
+    </UPageBody>
+  </div>
 </template>
--- app/pages/2/index.vue
+++ app/pages/2/index.vue
@@ -15,15 +15,13 @@
 </script>
 
 <template>
-  <UContainer>
-    <UPage>
-      <UPageHeader
-        :title="title"
-        description="Monster Buddy is a companion app for Monster Hunter Stories 2: Wings of Ruin that can help you learn or remember information useful during combat and general gameplay. It's not a complete guide, walkthrough or advanced character / gene planner (yet)."
-        :headline="headline"
-      />
+  <div>
+    <UPageHeader
+      :title="title"
+      description="Monster Buddy is a companion app for Monster Hunter Stories 2: Wings of Ruin that can help you learn or remember information useful during combat and general gameplay. It's not a complete guide, walkthrough or advanced character / gene planner (yet)."
+      :headline="headline"
+    />
 
-      <UPageBody>Content</UPageBody>
-    </UPage>
-  </UContainer>
+    <UPageBody>Content</UPageBody>
+  </div>
 </template>
