e5a7d7f1d1f68ac1dae0770570bddfd5ce06301a
te <te1@users.noreply.github.com>
2026-03-10 02:01:05 +0100

    mobile

--- app/components/app/AppTitle.vue
+++ app/components/app/AppTitle.vue
@@ -17,19 +17,35 @@
 </script>
 
 <template>
-  <UTooltip v-if="hasBack" text="Back" class="md:hidden">
-    <UButton
-      icon="ph:arrow-left"
-      color="neutral"
-      variant="ghost"
-      aria-label="Back"
-      @click="goBack"
-    />
-  </UTooltip>
+  <div class="relative grow lg:flex-1">
+    <div
+      class="absolute inset-y-0 -left-1.5 flex items-center transition-[transform,opacity] duration-150 ease-out will-change-transform md:hidden"
+      :class="
+        hasBack && isMobile
+          ? 'translate-x-0 opacity-100'
+          : 'pointer-events-none -translate-x-2 opacity-0'
+      "
+    >
+      <UTooltip text="Back">
+        <UButton
+          icon="ph:arrow-left"
+          color="neutral"
+          variant="ghost"
+          aria-label="Back"
+          :disabled="!hasBack"
+          :tabindex="hasBack ? 0 : -1"
+          @click="goBack"
+        />
+      </UTooltip>
+    </div>
 
-  <div class="text-lg font-medium tracking-wide dark:text-toned">
-    <span v-if="mobileHeader && isMobile" v-text="mobileHeader" />
+    <div
+      class="text-lg font-medium tracking-wide transition-transform duration-150 ease-out will-change-transform dark:text-toned"
+      :class="hasBack && isMobile ? 'translate-x-8' : 'translate-x-0'"
+    >
+      <span v-if="mobileHeader && isMobile" v-text="mobileHeader" />
 
-    <NuxtLink v-else to="/">Monster Buddy</NuxtLink>
+      <NuxtLink v-else to="/">Monster Buddy</NuxtLink>
+    </div>
   </div>
 </template>
