de57ea4f4e0f04b8f41d5b95cd2924354306310a
te <te1@users.noreply.github.com>
2026-03-15 19:52:42 +0100

    style

--- app/components/s2/monster/S2MonsterNoResults.vue
+++ app/components/s2/monster/S2MonsterNoResults.vue
@@ -5,17 +5,21 @@
 </script>
 
 <template>
-  <!-- TODO CSS -->
-
-  <div class="box flex flex-col items-center space-y-4 py-4">
-    <span class="text-2xl font-semibold text-neutral-500 dark:text-neutral-300">
+  <div class="box flex flex-col items-center gap-6 py-6">
+    <span class="text-2xl font-medium">
       <slot>No results found</slot>
     </span>
 
-    <UIcon class="h-16! w-16! text-neutral-400 dark:text-neutral-400" name="ph:smiley-sad-light" />
+    <UIcon class="h-16 w-16 text-dimmed" name="ph:smiley-sad-light" />
 
-    <UButton color="neutral" variant="soft" class="w-[160px]" @click="filterStore?.resetFilter">
-      Reset filter
-    </UButton>
+    <UButton
+      color="neutral"
+      variant="soft"
+      class="w-32"
+      trailingIcon="ph:arrow-counter-clockwise"
+      label="Reset filter"
+      :ui="{ base: 'justify-between font-normal', trailingIcon: 'text-dimmed' }"
+      @click="filterStore?.resetFilter"
+    />
   </div>
 </template>
