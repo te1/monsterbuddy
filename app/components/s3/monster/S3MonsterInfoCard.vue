c191723d4b0de9ed21e240577107a6937b14b734
te <te1@users.noreply.github.com>
2026-03-24 02:32:08 +0100

    pages

---
+++ app/components/s3/monster/S3MonsterInfoCard.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup>
+  import type { Monster } from '~/services/3/types';
+  import { formatMonsterInfo } from '~/services/3/presentation';
+
+  const props = defineProps<{ monster: Monster }>();
+
+  const info = computed(() => formatMonsterInfo(props.monster));
+  const hasLocations = computed(() => false); // TODO locations
+</script>
+
+<template>
+  <section class="flex flex-col gap-2">
+    <div class="flex justify-between">
+      <div>
+        <h3 class="sr-only">General Monster Info</h3>
+
+        <div class="-mb-1 text-muted" v-text="info" />
+
+        <div class="mb-1 text-2xl font-medium" v-text="monster.name" />
+
+        <div class="flex items-center gap-1">
+          <UTooltip text="Genus">
+            <UIcon name="ph:dna" class="text-muted" />
+          </UTooltip>
+
+          <span v-text="monster.genus" />
+        </div>
+
+        <!-- TODO habitat?
+        <div class="flex items-center gap-1">
+          <UTooltip text="Habitat">
+            <UIcon name="ph:map-pin-fill" class="text-dimmed" />
+          </UTooltip>
+
+          <span v-text="monster.habitat" />
+        </div>
+        -->
+
+        <div class="flex items-center gap-1">
+          <UTooltip text="Rank">
+            <UIcon name="ph:star-fill" class="text-dimmed" />
+          </UTooltip>
+
+          <span v-text="monster.rank" />
+        </div>
+      </div>
+
+      <div class="flex flex-col items-center gap-2">
+        <S3MonsterIcon :monster="monster" />
+
+        <UTooltip v-if="monster.hatchable" text="Hatchable">
+          <img
+            src="~/assets/icon/hatchable.svg"
+            alt="Hatchable"
+            width="32"
+            height="32"
+            class="size-8"
+          />
+        </UTooltip>
+      </div>
+    </div>
+
+    <div v-if="hasLocations">
+      <h3 class="text-lg font-medium">Locations</h3>
+
+      <!-- TODO locations
+      <S3MonsterLocation
+        v-for="location in monster.locations"
+        :key="location.main"
+        :location="location"
+      />
+      -->
+    </div>
+  </section>
+</template>
