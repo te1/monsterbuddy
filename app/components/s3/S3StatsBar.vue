58b2e0bdad96e2427f0d89b871ccb2eb6607785b
te <te1@users.noreply.github.com>
2026-03-24 01:44:37 +0100

    pages

---
+++ app/components/s3/S3StatsBar.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+  const props = withDefaults(
+    defineProps<{
+      value?: number;
+      label: string;
+    }>(),
+    {
+      value: undefined,
+    }
+  );
+
+  function getColor(n: number) {
+    if (props.value != null && props.value >= n) {
+      return 'bg-primary-600/75 dark:bg-primary-500/75';
+    }
+    return 'bg-neutral-500/25 dark:bg-neutral-400/25';
+  }
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <span class="w-18" v-text="label" />
+
+    <span class="flex flex-1 gap-0.5">
+      <div v-for="n in 10" :key="n" class="h-4 flex-1" :class="getColor(n)" />
+    </span>
+
+    <span class="w-4 text-right font-medium" v-text="value" />
+  </div>
+</template>
---
+++ app/components/s3/monstie/S3MonstieStatsCard.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+  import type { Monster } from '~/services/3/types';
+
+  const props = defineProps<{ monster: Monster }>();
+
+  const stats = computed(() => {
+    return props.monster?.stats?.base;
+  });
+
+  const hasStats = computed(() => {
+    return (
+      stats.value?.hp != null ||
+      stats.value?.defense != null ||
+      stats.value?.attack != null ||
+      stats.value?.crit != null ||
+      stats.value?.speed != null ||
+      stats.value?.recovery != null ||
+      stats.value?.stamina != null
+    );
+  });
+</script>
+
+<template>
+  <section v-if="hasStats" class="flex flex-col gap-3">
+    <h3 class="text-lg font-medium">Monstie Stats</h3>
+
+    <div v-if="stats?.hp != null || stats?.defense != null">
+      <div class="font-medium">Bulk</div>
+      <S3StatsBar :value="stats?.hp ?? undefined" label="HP" />
+      <S3StatsBar :value="stats?.defense ?? undefined" label="Defense" />
+    </div>
+
+    <div v-if="stats?.attack != null || stats?.speed != null || stats?.crit != null">
+      <div class="font-medium">Offense</div>
+      <S3StatsBar :value="stats?.attack ?? undefined" label="Attack" />
+      <S3StatsBar :value="stats?.speed ?? undefined" label="Speed" />
+      <S3StatsBar :value="stats?.crit ?? undefined" label="Crit" />
+    </div>
+
+    <div v-if="stats?.stamina != null || stats?.recovery != null">
+      <div class="font-medium">Stamina</div>
+      <S3StatsBar :value="stats?.stamina ?? undefined" label="Starting" />
+      <S3StatsBar :value="stats?.recovery ?? undefined" label="Recovery" />
+    </div>
+
+    <div v-if="stats?.bulk != null || stats?.total != null">
+      <div v-if="stats?.bulk != null" class="flex items-center gap-2">
+        <span class="w-18">Bulk</span>
+        <span class="flex-1">HP + Defense</span>
+        <span class="text-right font-medium" v-text="stats?.bulk" />
+      </div>
+
+      <div v-if="stats?.total != null" class="flex items-center gap-2">
+        <span class="w-18">Total</span>
+        <span class="flex-1">HP + Defense + Attack + Speed</span>
+        <span class="text-right font-medium" v-text="stats?.total" />
+      </div>
+    </div>
+  </section>
+</template>
