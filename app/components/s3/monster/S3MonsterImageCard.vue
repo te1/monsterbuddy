9abf0478b050e993650bb140a19841738e320f6c
te <te1@users.noreply.github.com>
2026-03-24 02:22:29 +0100

    pages

---
+++ app/components/s3/monster/S3MonsterImageCard.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  import type { Monster } from '~/services/3/types';
+
+  defineProps<{ monster: Monster }>();
+</script>
+
+<template>
+  <section>
+    <S3MonsterImage class="size-full max-h-[400px] object-contain" :monster="monster" />
+  </section>
+</template>
