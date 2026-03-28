e84aced299f2e79a4dc62fc4f27cc412babcbf5f
te <te1@users.noreply.github.com>
2026-03-28 18:38:51 +0100

    pages

--- app/components/s3/monster/S3MonsterFlavorCard.vue
+++ app/components/s3/monster/S3MonsterFlavorCard.vue
@@ -1,16 +1,51 @@
 <script lang="ts" setup>
   import type { Monster } from '~/services/3/types';
 
-  defineProps<{ monster: Monster }>();
+  const props = defineProps<{ monster: Monster }>();
 
-  // TODO handle mutation text
+  const flavors = computed(() => {
+    return Object.entries(props.monster.flavor ?? {}).map(([label, text]) => {
+      return {
+        label,
+        text,
+      };
+    });
+  });
+
+  const flavorTexts = computed(() =>
+    flavors.value.filter((flavor) => !flavor.label.startsWith('MUTATION'))
+  );
+
+  const mutationRequirements = computed(() =>
+    flavors.value.filter((flavor) => flavor.label.startsWith('MUTATION'))
+  );
 </script>
 
 <template>
-  <section v-if="monster.flavor" class="flex flex-col gap-3 text-toned">
-    <div v-for="(flavor, label) in monster.flavor" :key="label">
-      <strong v-if="label !== 'DEFAULT'" class="font-medium text-default">{{ label }}: </strong>
-      {{ flavor }}
+  <section
+    v-if="flavorTexts.length > 0 || mutationRequirements.length > 0"
+    class="flex flex-col gap-3 text-toned"
+  >
+    <div v-for="flavor in flavorTexts" :key="flavor.label">
+      <strong v-if="flavor.label !== 'DEFAULT'" class="font-medium text-default">
+        {{ flavor.label }}:
+      </strong>
+
+      {{ flavor.text }}
+    </div>
+
+    <div v-if="mutationRequirements.length > 0">
+      <div class="font-medium">Mutation Requirements</div>
+
+      <ProseUl>
+        <ProseLi
+          v-for="requirement in mutationRequirements"
+          :key="requirement.label"
+          class="leading-normal"
+        >
+          {{ requirement.text }}
+        </ProseLi>
+      </ProseUl>
     </div>
   </section>
 </template>
