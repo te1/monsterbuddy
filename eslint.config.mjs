69aa461a5d07cad622fa68fa102b28d5fba80a4b
te <te1@users.noreply.github.com>
2026-03-28 16:27:36 +0100

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,9 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default withNuxt(
   {
+    ignores: ['public/**', '.agents/**'],
+  },
+  {
     rules: {
       'vue/attribute-hyphenation': ['error', 'never'],
       'vue/v-on-event-hyphenation': ['error', 'never'],
