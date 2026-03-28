86cd97fa9b8e8c6ec9463a4ddb17e1004f77d8a2
te <te1@users.noreply.github.com>
2026-03-28 23:24:53 +0100

    style

--- app/layouts/sidebar3.vue
+++ app/layouts/sidebar3.vue
@@ -153,7 +153,7 @@
     </UPage>
 
     <div
-      class="pointer-events-none fixed right-[calc(-154px+var(--scrollbar-width,0px))] bottom-[-154px] -z-10 size-[448px] -rotate-23 overflow-hidden opacity-5 select-none lg:right-[calc(-192px+var(--scrollbar-width,0px))] lg:bottom-[-192px] lg:opacity-25"
+      class="pointer-events-none fixed right-[calc(-154px+var(--scrollbar-width,0px))] bottom-[-154px] -z-10 size-[448px] -rotate-23 overflow-hidden opacity-10 select-none lg:right-[calc(-192px+var(--scrollbar-width,0px))] lg:bottom-[-192px] lg:opacity-50 dark:opacity-5 dark:lg:opacity-25"
     >
       <img
         src="~/assets/app3.webp"
