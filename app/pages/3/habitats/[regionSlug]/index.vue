e790a0d833169857bd29fbfe0a8c7fbf438b8ad8
te <te1@users.noreply.github.com>
2026-03-31 23:39:20 +0200

    update icon

--- app/pages/3/habitats/[regionSlug]/index.vue
+++ app/pages/3/habitats/[regionSlug]/index.vue
@@ -230,7 +230,7 @@
             <div
               class="sticky top-(--ui-header-height) z-10 -mx-1 flex items-center bg-elevated/90 p-1 backdrop-blur dark:bg-muted/90"
             >
-              <UIcon name="ph:dna" class="w-6 text-muted" />
+              <UIcon name="ph:hash" class="w-6 text-muted" />
 
               <h2 class="font-semibold" v-text="group.key" />
             </div>
