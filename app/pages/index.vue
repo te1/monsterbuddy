8068c38413a07d3a934a14a2c05ad8350d015934
te <te1@users.noreply.github.com>
2026-04-01 23:09:52 +0200

    seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -73,7 +73,7 @@
 
   const features = [
     {
-      title: 'Monsters',
+      title: 'Monsterpedia',
       icon: 'ph:list-numbers',
       lines: ['Attack Patterns', 'Elemental Weaknesses', 'Weapon Effectiveness'],
     },
