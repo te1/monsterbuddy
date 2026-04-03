447782a357a9c2bbb950b637670faa745e12dd19
te <te1@users.noreply.github.com>
2026-04-04 00:05:03 +0200

    more gene info

--- data/3/genes.schema.ts
+++ data/3/genes.schema.ts
@@ -10,6 +10,53 @@ export type GeneElement = z.infer<typeof GeneElementSchema>;
 export const GeneSizeSchema = z.enum(['none', 'S', 'M', 'L', 'XL', '+']);
 export type GeneSize = z.infer<typeof GeneSizeSchema>;
 
+export const GeneTargetSchema = z.enum([
+  'allAllies',
+  'allEnemies',
+  'randomEnemy',
+  'singleEnemy',
+  'user',
+]);
+export type GeneTarget = z.infer<typeof GeneTargetSchema>;
+
+export const GeneStatusEffectSchema = z.enum([
+  '1HitKo',
+  'accuracyDown',
+  'attackDown',
+  'attackUp',
+  'blastblight',
+  'bleeding',
+  'burn',
+  'criticalUp',
+  'darkness',
+  'defenseDown',
+  'defenseUp',
+  'dodgeUp',
+  'dragonAttackUp',
+  'dragonDefenseDown',
+  'evasionDown',
+  'fireAttackUp',
+  'fireDefenseUp',
+  'iceAttackUp',
+  'inflictRateUp',
+  'noxiousPoison',
+  'paralysis',
+  'poison',
+  'regenerate',
+  'severePoison',
+  'sleep',
+  'speedDown',
+  'speedUp',
+  'staminaRegenUp',
+  'stun',
+  'thunderAttackUp',
+  'thunderDefenseDown',
+  'waterAttackUp',
+  'wyvernfellUp',
+  'wyvernsoulDefenseDown',
+]);
+export type GeneStatusEffect = z.infer<typeof GeneStatusEffectSchema>;
+
 export const GeneSchema = z.object({
   name: z.string(),
   slug: z.string(),
@@ -21,6 +68,10 @@ export const GeneSchema = z.object({
   power: z.number().nullable(),
   wyvernfell: z.number().nullable(),
   description: z.string(),
+  target: GeneTargetSchema.nullable(),
+  breath: z.boolean(),
+  eggSkill: z.boolean(),
+  statusEffects: z.array(GeneStatusEffectSchema),
   sizes: z.array(z.string()),
 });
 export type Gene = z.infer<typeof GeneSchema>;
