b9a97026124f334ade49014ceca3b21ae11bc05d
te <te1@users.noreply.github.com>
2026-04-07 01:43:35 +0200

    wip

---
+++ app/components/s3/gene/S3GeneDetailsSidebar.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+  import { genesBySlug } from '~/services/3/genes';
+  import useGeneHistoryStore from '~/stores/3/geneHistoryStore';
+
+  const history = useGeneHistoryStore();
+
+  const route = useRoute();
+
+  const gene = computed(() => {
+    const slug = route.params.slug;
+
+    if (typeof slug !== 'string') {
+      return undefined;
+    }
+
+    return genesBySlug.get(slug);
+  });
+
+  const isGenePinned = computed(() => {
+    return history.isGenePinned(gene.value?.slug ?? '');
+  });
+
+  function toggleGenePin() {
+    if (gene.value) {
+      history.togglePinnedGene(gene.value.slug);
+    }
+  }
+
+  const tabs = [{ label: 'Actions', slot: 'actions' }];
+</script>
+
+<template>
+  <UTabs color="neutral" variant="link" :items="tabs">
+    <template #actions>
+      <div class="flex flex-col gap-0">
+        <ClientOnly>
+          <AppPinToggle
+            :modelValue="isGenePinned"
+            subject="gene"
+            @update:modelValue="toggleGenePin"
+          />
+
+          <template #fallback>
+            <USkeleton v-for="i in 3" :key="i" class="my-1 h-6 w-[80%]" />
+          </template>
+        </ClientOnly>
+      </div>
+    </template>
+  </UTabs>
+</template>
