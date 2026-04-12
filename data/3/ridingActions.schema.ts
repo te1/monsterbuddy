06e8ad97ba4416c8ec0647e2fce44ee692862dca
te <te1@users.noreply.github.com>
2026-04-12 22:45:14 +0200

    add descriptions for some riding actions, fix link

--- data/3/ridingActions.schema.ts
+++ data/3/ridingActions.schema.ts
@@ -10,6 +10,7 @@ export type RidingActionType = z.infer<typeof RidingActionTypeSchema>;
 
 export const RidingActionSchema = z.object({
   name: z.string(),
+  description: z.string().optional(), // TODO remove optional after all data is gathered
   type: RidingActionTypeSchema,
   slug: z.string(),
 });
