df259b3c3f44ad2a7238dce21f1a7dccf7f548fa
te <te1@users.noreply.github.com>
2026-04-16 01:12:11 +0200

    buff / debuff numbers

--- data/3/buffs.schema.ts
+++ data/3/buffs.schema.ts
@@ -11,8 +11,16 @@ export const BuffDetailTypeSchema = z.enum([
   'damageDone',
   'damageTaken',
   'critRate',
+  'evasionRate',
+  'accuracyRate',
   'maxHpRecovery',
+  'rawSpeed',
+  'rawStaminaRegen',
+  'wyvernfell',
+  'wyvernsoulDamageTaken',
   'leech',
+  'ailmentInflictRate',
+  'effectChance',
 ]);
 export type BuffDetailType = z.infer<typeof BuffDetailTypeSchema>;
 
