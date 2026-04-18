08a67853ba6f14c999a287388b0f3e5ac0ba02fd
te <te1@users.noreply.github.com>
2026-04-18 23:11:39 +0200

    egg power icon colors

--- app/components/s3/eggPower/S3EggPowerIcon.vue
+++ app/components/s3/eggPower/S3EggPowerIcon.vue
@@ -1,24 +1,53 @@
 <script lang="ts" setup>
   import type { EggPower } from '~/services/3/types';
 
-  defineProps<{
+  const props = defineProps<{
     eggPower: EggPower;
     awakened?: boolean;
     big?: boolean;
     contrast?: boolean;
   }>();
+
+  const bgColor = computed(() => {
+    if (props.awakened) {
+      switch (props.eggPower.rank) {
+        case 'S':
+          return 'bg-fuchsia-500/25 dark:bg-fuchsia-400/15';
+
+        case 'A':
+          return 'bg-emerald-500/25 dark:bg-emerald-400/15';
+
+        case 'B':
+          return 'bg-sky-500/25 dark:bg-sky-400/15';
+      }
+    }
+
+    return props.contrast ? 'bg-accented' : 'bg-default';
+  });
+
+  const textColor = computed(() => {
+    switch (props.eggPower.rank) {
+      case 'S':
+        return props.awakened ? 'text-fuchsia-600 dark:text-fuchsia-200' : 'text-fuchsia-400';
+
+      case 'A':
+        return props.awakened ? 'text-emerald-600 dark:text-emerald-200' : 'text-emerald-400';
+
+      case 'B':
+        if (!props.contrast) {
+          return 'text-muted';
+        }
+        return props.awakened ? 'text-sky-600 dark:text-sky-200' : 'text-sky-400';
+    }
+
+    return 'text-muted';
+  });
 </script>
 
 <template>
   <div
     class="flex items-center justify-center rounded font-semibold"
-    :class="{
-      'size-10 text-lg': big,
-      'size-7 text-sm': !big,
-      'bg-accented text-toned': contrast,
-      'bg-default text-muted': !contrast,
-      'bg-emerald-300 dark:bg-emerald-900': awakened,
-    }"
+    :class="[big ? 'size-10 text-lg' : 'size-7 text-sm', textColor, bgColor]"
     v-text="eggPower.rank"
   />
 </template>
