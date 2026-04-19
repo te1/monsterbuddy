5f247381f0bfc197a5ef3f6e48668edd8e398800
te <te1@users.noreply.github.com>
2026-04-19 23:37:49 +0200

    fix composable

--- app/composables/3/useMonstieBuild.ts
+++ app/composables/3/useMonstieBuild.ts
@@ -1,22 +1,36 @@
-import type { MonstieBuild } from '~/services/3/monstieBuilds';
 import { liveQuery } from 'dexie';
+import { MonstieBuild } from '~/services/3/monstieBuilds';
 import { db } from '~/services/3/localDb';
 
-export function useMonstieBuild(id: string) {
+export function useMonstieBuild(id: MaybeRefOrGetter<string | undefined>) {
   const data = ref<MonstieBuild | undefined>(undefined);
 
-  const subscription = liveQuery(() => db.monstieBuilds.get(id)).subscribe({
-    next(value) {
-      data.value = value?.data;
-    },
-    error(err) {
-      console.error(`useMonstieBuild(${id}) error`, err);
-    },
+  watchEffect((onCleanup) => {
+    data.value = undefined;
+
+    if (!import.meta.client) {
+      return;
+    }
+
+    const currentId = toValue(id);
+
+    if (!currentId) {
+      return;
+    }
+
+    const subscription = liveQuery(() => db.monstieBuilds.get(currentId)).subscribe({
+      next(value) {
+        data.value = value ? MonstieBuild.fromEntity(value) : undefined;
+      },
+      error(err) {
+        console.error(`useMonstieBuild(${currentId}) error`, err);
+      },
+    });
+
+    onCleanup(() => {
+      subscription.unsubscribe();
+    });
   });
 
-  onScopeDispose(() => {
-    subscription.unsubscribe();
-  });
-
-  return data;
+  return readonly(data);
 }
