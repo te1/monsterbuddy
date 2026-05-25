c9ba8d615958f7456096908fc4b9f5f8c1e53491
te <te1@users.noreply.github.com>
2026-05-25 14:30:12 +0200

    move drag avatar up when dragging by touch

--- app/composables/3/useGeneGridDrag.ts
+++ app/composables/3/useGeneGridDrag.ts
@@ -7,7 +7,7 @@ const geneIndexes: readonly GeneIndex[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const dragThreshold = 12;
 const touchDragThreshold = 24;
-const touchTargetOffset = 32;
+const touchTargetOffset = -32;
 
 export function useGeneGridDrag({
   editMode,
